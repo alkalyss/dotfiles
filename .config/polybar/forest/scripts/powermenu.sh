0ce1da0b9026c68df23838301efba05d1e0d9e60
Alkinoos Alyssandrakis <alk.alyssandrakis@gmail.com>
2021-04-04 13:25:22 +0300

    tweak polybar setup

--- .config/polybar/forest/scripts/powermenu.sh
+++ .config/polybar/forest/scripts/powermenu.sh
@@ -36,26 +36,8 @@ options="$lock\n$suspend\n$logout\n$reboot\n$shutdown"
 
 chosen="$(echo -e "$options" | $rofi_command -p "Uptime: $uptime" -dmenu -selected-row 0)"
 case $chosen in
-    $shutdown)
-		ans=$(confirm_exit &)
-		if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
-			systemctl poweroff
-		elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
-			exit 0
-        else
-			msg
-        fi
-        ;;
-    $reboot)
-		ans=$(confirm_exit &)
-		if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
-			systemctl reboot
-		elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
-			exit 0
-        else
-			msg
-        fi
-        ;;
+    $shutdown) systemctl poweroff ;;
+    $reboot) systemctl reboot ;;
     $lock)
 		if [[ -f /usr/bin/i3lock ]]; then
 			i3lock
@@ -64,31 +46,19 @@ case $chosen in
 		fi
         ;;
     $suspend)
-		ans=$(confirm_exit &)
-		if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
-			mpc -q pause
-			amixer set Master mute
-			systemctl suspend
-		elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
-			exit 0
-        else
-			msg
-        fi
+		mpc -q pause
+		amixer set Master mute
+		systemctl suspend
         ;;
     $logout)
-		ans=$(confirm_exit &)
-		if [[ $ans == "yes" || $ans == "YES" || $ans == "y" || $ans == "Y" ]]; then
-			if [[ "$DESKTOP_SESSION" == "Openbox" ]]; then
-				openbox --exit
-			elif [[ "$DESKTOP_SESSION" == "bspwm" ]]; then
-				bspc quit
-			elif [[ "$DESKTOP_SESSION" == "i3" ]]; then
-				i3-msg exit
-			fi
-		elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
-			exit 0
-        else
-			msg
-        fi
+		if [[ "$DESKTOP_SESSION" == "Openbox" ]]; then
+			openbox --exit
+		elif [[ "$DESKTOP_SESSION" == "bspwm" ]]; then
+			bspc quit
+		elif [[ "$DESKTOP_SESSION" == "i3" ]]; then
+			i3-msg exit
+		elif [[ "$DESKTOP_SESSION" == "leftwm" ]]; then
+			pkill leftwm
+		fi
         ;;
 esac
