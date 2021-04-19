33928b3a12b6b62654e35b1cea430493fdd01bb6
Alkinoos Alyssandrakis <alk.alyssandrakis@gmail.com>
2021-04-19 14:27:21 +0300

    change polybar config

--- .config/polybar/forest/scripts/layouts.sh
+++ .config/polybar/forest/scripts/layouts.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-layouts=("CenterMain" "CenterMainBalanced" "EvenHorizontal" "EvenVertical" "Fibonacci" "GridHorizontal" "MainAndVertStack")
+layouts=("CenterMain" "CenterMainBalanced" "EvenHorizontal" "EvenVertical" "Fibonacci" "GridHorizontal" "MainAndVertStack" "MainAndHorizontalStack" "MainAndDeck" "Monocle" "RightWiderLeftStack" "LeftWiderRightStack")
 
-icons=("Center Main" "Center Main Balanced" "Even Horizontal" "Even Vertical" "Fibonacci" "Grid Horizontal" "Main & Stack")
+icons=("頻" "冀" "𤋮" "ﰧ" "侀" "全" "D" "Main & H Stack" "Main & Deck" "" "R>L Stack" "DD")
 
 layout=$(leftwm-state -q | jq -r '.["workspaces"] | .[0].layout')
 
@@ -14,4 +14,9 @@ case $layout in
 	${layouts[4]}) echo ${icons[4]};;
 	${layouts[5]}) echo ${icons[5]};;
 	${layouts[6]}) echo ${icons[6]};;
+	${layouts[7]}) echo ${icons[7]};;
+	${layouts[8]}) echo ${icons[8]};;
+	${layouts[9]}) echo ${icons[9]};;
+	${layouts[10]}) echo ${icons[10]};;
+	${layouts[11]}) echo ${icons[11]};;
 esac
