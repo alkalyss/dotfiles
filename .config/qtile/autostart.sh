ae6c622ac36cf0dfe339b70ced8ae983c3c5185b
Alkinoos Alyssandrakis <alk.alyssandrakis@gmail.com>
2021-11-24 12:11:11 +0200

    reorganise polybar config

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 killall polybar
-polybar -q qtile -c "${XDG_CONFIG:-$HOME/.config}/polybar/default/config.ini" &
+polybar -q laptop -c "${XDG_CONFIG:-$HOME/.config}/polybar/default/config.ini" &
