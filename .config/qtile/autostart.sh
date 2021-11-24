c2b5809ac5c6312307631d5af4dec7d85bfb3464
Alkinoos Alyssandrakis <alk.alyssandrakis@gmail.com>
2021-11-24 12:37:22 +0200

    unify desktop and laptop configs

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
 killall polybar
-polybar -q laptop -c "${XDG_CONFIG:-$HOME/.config}/polybar/default/config.ini" &
+if [[ $HOSTNAME == "laptop" ]]; then
+	polybar -q laptop -c "${XDG_CONFIG:-$HOME/.config}/polybar/default/config.ini" &
+else
+	polybar -q desktop -c "${XDG_CONFIG:-$HOME/.config}/polybar/default/config.ini" &
+fi
