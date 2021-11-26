8af9ba390baa1b6a8962f195e36fc5256c5e3242
Alkinoos Alyssandrakis <alk.alyssandrakis@gmail.com>
2021-11-26 19:46:48 +0200

    fix polybar launch and settings

--- .config/qtile/autostart.sh
+++ .config/qtile/autostart.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 killall polybar
-if [[ $HOSTNAME == "laptop" ]]; then
+if [[ $(hostname) == "laptop" ]]; then
 	polybar -q laptop -c "${XDG_CONFIG:-$HOME/.config}/polybar/default/config.ini" &
 else
 	polybar -q desktop -c "${XDG_CONFIG:-$HOME/.config}/polybar/default/config.ini" &
