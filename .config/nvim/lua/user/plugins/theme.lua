c162892415676d9fcc7f409c9de18b91f931d99b
Alkinoos Alyssandrakis <alk.alyssandrakis@gmail.com>
2023-01-08 16:21:44 +0200

    make vim background transparent

--- .config/nvim/lua/user/plugins/theme.lua
+++ .config/nvim/lua/user/plugins/theme.lua
@@ -4,3 +4,6 @@ vim.g.equinusocio_material_bracket_improved = 0
 vim.cmd [[
 	colorscheme equinusocio_material
 ]]
+
+vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
+vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
