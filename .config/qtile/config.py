6de5acea2a789f3eca4ca2fb7a338a6a6abe6cb2
Alkinoos Alyssandrakis <alk.alyssandrakis@gmail.com>
2023-06-12 17:36:28 +0300

    add keybinds for audio and media controll

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -104,6 +104,14 @@ keys = [
     Key([mod, "shift"], "f", lazy.window.toggle_floating()),
     Key([mod], "w", lazy.to_screen(0)),
     Key([mod], "e", lazy.to_screen(1)),
+
+    Key([], "XF86AudioPlay", lazy.spawn("playerctl play-pause")),
+    Key([], "XF86AudioPrev", lazy.spawn("playerctl previous")),
+    Key([], "XF86AudioNext", lazy.spawn("playerctl next")),
+
+    Key([], "XF86AudioMute", lazy.spawn("pactl set-sink-mute @DEFAULT_SINK@ toggle")),
+    Key([], "XF86AudioLowerVolume", lazy.spawn("pactl set-sink-volume @DEFAULT_SINK@ -5%")),
+    Key([], "XF86AudioRaiseVolume", lazy.spawn("pactl set-sink-volume @DEFAULT_SINK@ +5%")),
 ]
 
 groups = [Group(i) for i in "12345"]
