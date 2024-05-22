3d47c43fb54ac0c7983981321c6fe670f6f42616
Alkinoos Alyssandrakis <alk.alyssandrakis@gmail.com>
2024-05-22 21:53:35 +0300

    seamless navigation between nvim and tmux

---
+++ .config/nvim/lua/alkalyss/plugins/tmux.lua
@@ -0,0 +1,22 @@
+return {
+	"christoomey/vim-tmux-navigator",
+	event = "VeryLazy",
+	cmd = {
+		"TmuxNavigateLeft",
+		"TmuxNavigateDown",
+		"TmuxNavigateUp",
+		"TmuxNavigateRight",
+		"TmuxNavigatePrevious",
+	},
+	keys = {
+		{ "<c-h>", ":TmuxNavigateLeft<cr>" },
+		{ "<c-j>", ":TmuxNavigateDown<cr>" },
+		{ "<c-k>", ":TmuxNavigateUp<cr>" },
+		{ "<c-l>", ":TmuxNavigateRight<cr>" },
+		{ "<c-\\>", ":TmuxNavigatePrevious<cr>" },
+	},
+	config = function()
+		vim.g.tmux_navigator_save_on_switch = 2
+		vim.g.tmux_navigator_disable_when_zoomed = 1
+	end,
+}
