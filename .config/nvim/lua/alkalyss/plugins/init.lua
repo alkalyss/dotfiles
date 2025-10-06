7e1dd244e4b99fa1fa6caad4e5428a3c48e837d0
Alkinoos Alyssandrakis <alk.alyssandrakis@gmail.com>
2025-10-06 13:30:01 +0300

    nvim: disable copilot

--- .config/nvim/lua/alkalyss/plugins/init.lua
+++ .config/nvim/lua/alkalyss/plugins/init.lua
@@ -7,7 +7,7 @@ return {
 	'godlygeek/tabular', -- Allign items
 	'jessarcher/vim-heritage', -- Create missing parent directories when creating a new file
 	'sickill/vim-pasta', -- Context aware pasting
-	"github/copilot.vim", -- I think this explains itself
+	-- "github/copilot.vim", -- I think this explains itself
 
 	{ 'lukas-reineke/indent-blankline.nvim', main = "ibl", opts = {} }, -- Indent guides
 	{ 'windwp/nvim-autopairs', config = true } -- Auto bracket pairs
