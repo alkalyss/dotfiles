7b8390a8b4bb41263acd730f3bb97a21f878faba
Alkinoos Alyssandrakis <alk.alyssandrakis@gmail.com>
2025-10-06 13:26:21 +0300

    nvim: fix transparency

--- .config/nvim/lua/alkalyss/plugins/theme.lua
+++ .config/nvim/lua/alkalyss/plugins/theme.lua
@@ -4,12 +4,13 @@ return {
 	name = "catppuccin",
 	priority = 900,
 	init = function ()
-		vim.api.nvim_set_hl(0, "Normal", {bg = "none"})
-		vim.api.nvim_set_hl(0, "NormalFloat", {bg = "none"})
 		vim.cmd.colorscheme("catppuccin")
 	end,
 	opts = {
 		flavour = "mocha",
 		transparent_background = true,
+		float = {
+			transparent = true
+		}
 	}
 }
